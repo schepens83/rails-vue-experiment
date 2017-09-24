662c5d515cb2523c98ead4b6996aea36fd0993df
schepens83 <schepens@gmail.com>
2017-09-24 14:23:11 +0200

    add bootstrap

--- app/javascript/packs/calculator/main.vue
+++ app/javascript/packs/calculator/main.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="calculator">
-    <div id="">
+    <div class="row">
+      <div id="">
 
+      </div>
     </div>
-    <div id="numbers">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
-      <div>0</div>
+
+    <div class="row" id="numbers">
+      <div class="col-md-4">1</div>
+      <div class="col-md-4">2</div>
+      <div class="col-md-4">3</div>
+      <div class="col-md-4">4</div>
+      <div class="col-md-4">5</div>
+      <div class="col-md-4">6</div>
+      <div class="col-md-4">7</div>
+      <div class="col-md-4">8</div>
+      <div class="col-md-4">9</div>
+      <div class="col-md-4">0</div>
     </div>
-    <div id="signs">
-      <div>+</div>
-      <div>-</div>
+
+    <div class="row" id="signs">
+      <div class="col-md-6">+</div>
+      <div class="col-md-6">-</div>
     </div>
   </div>
 </template>
