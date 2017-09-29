bfd5b719ecd86b81e066adf95b4b7e3b0d29e1a2
schepens83 <schepens@gmail.com>
2017-09-29 10:14:16 +0200

    add migration for day_scores and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170929072232) do
+
+  create_table "day_scores", force: :cascade do |t|
+    t.date "day"
+    t.integer "tension"
+    t.integer "sleep"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
